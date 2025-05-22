647dbd6250463c66462794f451fce4ca0d76f7d4
Zakharova <oa.zakharova@mail.ru>
2025-05-22 18:56:46 +0300

    Перенос заготовки и замена на JPA

---
+++ hw06-jpa/src/main/java/ru/otus/hw/models/Comment.java
@@ -0,0 +1,24 @@
+package ru.otus.hw.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "comments")
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "text")
+    private String text;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "book_id")
+    private Book book;
+}
